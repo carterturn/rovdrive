64aea3b0e65edb54d7a5f452392791e5e619743b
Carter Turnbaugh <carterturn@tutanota.de>
2016-11-23 18:44:16 -0800

    Added license to watchdog

--- watchdog.h
+++ watchdog.h
@@ -1,3 +1,21 @@
+/*
+  Copyright 2016 Carter Turnbaugh
+
+  This file is part of Terca ROV Drive.
+
+  Terca ROV Drive is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Terca ROV Drive is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Terca ROV Drive.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #pragma once
 
 #include <thread>
